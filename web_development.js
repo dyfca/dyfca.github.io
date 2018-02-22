5e6224900caca34389f78791b249d670372cff8e
crazystir <guoqt3@mail2.sysu.edu.cn>
2018-02-22 11:50:37 -0600

    Chosen process

---
+++ web_development.js
@@ -0,0 +1,16 @@
+var currentProcess = 1
+var maxId = 7;
+var prefix="location.href='https://www.w3schools.com/"
+var suffix="/default.asp'"
+var data = ["xml", "html", "css", "js", "jquery", "angular", "bootstrap"]
+function enableClick(id) {
+	if (id > maxId)
+		return;
+	var str = "process-" + id.toString();
+	var ele = document.getElementById(str);
+	ele.childNodes[3].setAttribute("onclick", prefix+data[id-1]+suffix);
+	ele.childNodes[5].setAttribute("onclick", "enableClick("+(id+1).toString()+")");
+	ele.childNodes[3].classList.remove("disabled");
+	ele.childNodes[5].classList.remove("disabled");
+	id++;
+}
