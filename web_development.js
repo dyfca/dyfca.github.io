e912c2f7ecb66620693d301d9a407c1eb0fd19df
crazystir <guoqt3@mail2.sysu.edu.cn>
2018-03-01 14:28:47 -0600

    Update

--- web_development.js
+++ web_development.js
@@ -8,7 +8,10 @@ function enableClick(id) {
 		return;
 	var str = "process-" + id.toString();
 	var ele = document.getElementById(str);
-	ele.childNodes[3].setAttribute("onclick", prefix+data[id-1]+suffix);
+	if (id != 2)
+		ele.childNodes[3].setAttribute("onclick", prefix+data[id-1]+suffix);
+	else
+		ele.childNodes[3].setAttribute("onclick", "window.location = './info.html'; return false;");
 	ele.childNodes[5].setAttribute("onclick", "enableClick("+(id+1).toString()+")");
 	ele.childNodes[3].classList.remove("disabled");
 	ele.childNodes[5].classList.remove("disabled");
